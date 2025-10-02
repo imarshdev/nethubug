ebca57df930a672ca4cb687ecce210b2ca218602
imarshdev <forevermarsh004@gmail.com>
2025-10-02 20:27:04 +0300

    build a biitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import TopBar from "./components/blocks/0.topbar";
 import Dashboard from "./components/blocks/1.dashboard";
 import Income from "./components/blocks/2.income";
 
 export default function App() {
   return (
     <div className="container">
+      <TopBar />
       <div className="content">
         <Dashboard />
         <Income />
---
+++ src/components/blocks/0.topbar.jsx
@@ -0,0 +1,18 @@
+export default function TopBar() {
+  return (
+    <div
+      className="top-bar"
+      style={{
+        height: "40px",
+        padding: "10px",
+        background: "#000",
+        color: "#fff",
+        textAlign: "center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+      }}
+    ></div>
+  );
+}
